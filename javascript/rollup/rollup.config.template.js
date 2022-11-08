9ed6cf779f511a94725859dcd8aa07f973d7130f
Adam Dierkens <Adam_Dierkens@intuit.com>
2022-11-08 15:03:07 -0800

    Start adding support for esm only builds

--- javascript/rollup/rollup.config.template.js
+++ javascript/rollup/rollup.config.template.js
@@ -30,11 +30,15 @@ if (isBinBuild) {
 } else {
   builds = [
     bundle({
-      plugins: [esbuild(), styles({
-        modules: true,
-      }), json()],
+      plugins: [
+        esbuild(),
+        styles({
+          modules: true,
+        }),
+        json()
+      ],
       output: [
-        {
+        !TMPL_esm_only && {
           file: path.join('TMPL_out_dir', 'index.cjs.js'),
           format: 'cjs',
           sourcemap: true,
@@ -46,7 +50,7 @@ if (isBinBuild) {
           sourcemap: true,
           assetFileNames: "[name]-[hash][extname]",
         },
-      ],
+      ].filter(Boolean),
     }),
     bundle({
       plugins: [dts({
